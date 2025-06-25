cb482abfaf60a2ff5d1ffc7482a6c436fc488067
renshan <renshan@2155.io>
2025-06-25 20:07:16 +0800

    ✅ feat: 完成一个基本的在桌面显示的功能

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,85 @@
+use rdev::{listen, Event, EventType};
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use tauri::{Emitter, Manager, WebviewWindow};
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn always_on_top(window: tauri::Window, top: bool) -> String {
+    match window.set_always_on_top(top) {
+        Ok(_) => String::from("ok"),
+        Err(err) => err.to_string(),
+    }
+}
+
+// 定义一个结构体来表示我们想发送到前端的键盘事件
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct KeyboardEvent {
+    key: String,
+    name: String,
+    event_type: String,
+}
+
+// 事件监听函数
+fn start_keyboard_listener(window: WebviewWindow) {
+    let window_arc = Arc::new(Mutex::new(window));
+
+    thread::spawn(move || {
+        let callback = move |event: Event| {
+            let window = window_arc.lock().unwrap();
+            let mut ke = KeyboardEvent {
+                key: "".to_string(),
+                name: "".to_string(),
+                event_type: "".to_string(),
+            };
+
+            match event.event_type {
+                EventType::KeyPress(key) => {
+                    ke.event_type = "KeyPress".to_string();
+                    ke.key = format!("{:?}", key)
+                }
+                EventType::KeyRelease(key) => {
+                    ke.key = format!("{:?}", key);
+                    ke.event_type = "KeyRelease".to_string();
+                }
+                _ => return, // 忽略其他事件类型 (MouseMove, ButtonPress等)
+            };
+
+            if let Some(name) = event.name {
+                ke.key = name;
+            }
+
+            // 发送事件到前端
+            window.emit("keyboard-event", ke).unwrap();
+        };
+
+        // 开始监听事件
+        if let Err(error) = listen(callback) {
+            println!("Error: {:?}", error);
+        }
+    });
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
+        .plugin(tauri_plugin_process::init())
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap(); // 获取主窗口
+                                                                  // 在应用启动时开始监听键盘事件
+            start_keyboard_listener(window);
+            Ok(())
+        })
+        .plugin(tauri_plugin_single_instance::init(|_, _, _| {}))
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet, always_on_top])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,128 @@
+import { invoke } from "@tauri-apps/api/core";
+import { Event, listen } from "@tauri-apps/api/event";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { moveWindow, Position } from "@tauri-apps/plugin-positioner";
+import { exit, relaunch } from "@tauri-apps/plugin-process";
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  let strings: string[] = [];
+  let [noti, setNoti] = useState<string[]>([]);
+
+  const disableContextMenu = useCallback(() => {
+    document.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
+    });
+  }, []);
+
+  const disableCopy = useCallback(() => {
+    document.addEventListener("copy", function (e) {
+      e.preventDefault();
+    });
+  }, []);
+
+  const listener = (
+    event: Event<{ key: string; name: string; event_type: string }>,
+  ) => {
+    const payload = event.payload;
+    if (payload.event_type == "KeyPress") {
+      strings = [...strings];
+      strings.push(payload.key);
+      strings = strings.slice(-5).map((x) => {
+        if (x == "\b") return "Backspc";
+        if (x == "\u001b") return "Esc";
+        if (x == "ShiftLeft" || x == "ShiftRight") {
+          return "Shift";
+        }
+
+        // 处理ControlLeft和ControlRight
+        if (x.startsWith("Control")) {
+          return "Ctrl";
+        }
+
+        if (x.endsWith("Arrow")) {
+          switch (x.replace(/Arrow$/, "").toLowerCase()) {
+            case "left":
+              return "←";
+            case "right":
+              return "→";
+            case "up":
+              return "↑";
+            case "down":
+              return "↓";
+          }
+        }
+
+        // 处理空格键
+        if (x.length == 1 && x.charCodeAt(0) == 32) {
+          return "Space";
+        }
+
+        if (x == "\t") {
+          return "Tab";
+        }
+
+        if (x == "\r") {
+          return "Enter";
+        }
+
+        return x;
+      });
+
+      let filtered = strings
+        .filter((x) => !x.startsWith("Kp")) // 过滤小数字键盘
+        .filter((x) => !x.startsWith("Unknow")); // 过滤全键盘的Meta和Control左边的键
+
+      setNoti(filtered);
+    }
+  };
+
+  const setupEventListener = async () => {
+    const re = await listen("keyboard-event", listener);
+    return re;
+  };
+
+  const alwaysOnTop = useCallback(async () => {
+    const window = getCurrentWindow();
+    const top = true;
+    invoke("always_on_top", { window, top });
+  }, []);
+
+  const listenQuitEvent = useCallback(() => {
+    window.addEventListener("keydown", (evt) => {
+      if (evt.code == "q") {
+        exit(0);
+      }
+
+      if (evt.code == "r") {
+        relaunch();
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    moveWindow(Position.BottomCenter);
+
+    disableCopy();
+    disableContextMenu();
+    let r = setupEventListener();
+    alwaysOnTop();
+    listenQuitEvent();
+    return () => {
+      r.then();
+    };
+  }, []);
+
+  return (
+    <main className="container" data-tauri-drag-region>
+      <div style={{ color: "#fff" }}>
+        {noti.map((x) => (
+          <span style={{ margin: "0 4px" }}>{x}</span>
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export default App;
